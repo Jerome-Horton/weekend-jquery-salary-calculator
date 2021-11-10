775e7e3a802c87864a92274cba685316386c300b
Jerome Horton <jeromehorton1@gmail.com>
2021-11-09 22:36:30 -0600

    created click funtion for submit button

--- salary.js
+++ salary.js
@@ -1,3 +1,24 @@
-$(document).ready()
+$(document).ready(readyNow);
 console.log('JQ');
 
+function readyNow(){
+    $('#submitButton').on('click', newInfo);
+
+}
+
+
+// create a function to pass on the new info from the subtmit button.
+function newInfo(event){
+    console.log('new info!');
+// create an object to contain the information from the input fields.
+    let addEmployess = {
+        firstname: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        id: $('#id').val(),
+        title: $('#title').val(),
+        annaulSalary: $('#annaulSalary').val(),  
+    }
+}
+
+    
+}
